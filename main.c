49e1c274aebca0dee3f45515be888d1cfe53a5ba
Luana Lima <luanalima.2006@hotmail.com>
2024-10-27 20:29:42 -0300

    feat: relatório que lista as disciplinas detalhadamente

--- main.c
+++ main.c
@@ -952,10 +952,9 @@ int main() {
                       for (int j = 0; j < nomeTamanho; j++) {
                         disciplina[disciplinaEscolhida - 1].alunos[k][j] = alunos[alunoSelecionado - 1].nome[j];
                       }
+                      break;
                     }
                   }
-
-                  break;
                 }
               }
             }
@@ -1020,7 +1019,7 @@ int main() {
             else {
               temDisciplina = true;
               puts(disciplina[i].nome);
-              printf("- código: %d\n- semestre: %d\n- professor:", disciplina[i].codigo, disciplina[i].semestre);
+              printf("- Código: %d\n- Semestre: %d\n- Professor:", disciplina[i].codigo, disciplina[i].semestre);
               puts(disciplina[i].professor);
               printf("\n\n");
             }
@@ -1036,7 +1035,59 @@ int main() {
         }
         // Listar uma disciplina com detalhes
         else if (escolhaRelatorio == 4) {
-          printf("%d", escolhaRelatorio);
+          int temDisciplina = false;
+          system("clear");
+          for (int i = 0; i < disciplinaQtd; i++) {
+            if (disciplina[i].codigo == 0)
+              continue;
+
+            else {
+              temDisciplina = true;
+              printf("[%d] ", i + 1);
+              puts(disciplina[i].nome);
+            }
+          }
+
+          if (!temDisciplina) {
+            printf("NÃO HÁ DISCIPLINAS DISPONÍVEIS\n(Aperte Enter para continuar)\n\n");
+            getchar();
+          }
+          else {
+            int disciplinaEscolhida;
+            printf("\n\nQual disciplina deseja visualizar? (selecione o id): ");
+            scanf("%d", &disciplinaEscolhida);
+            getchar();
+
+            if (disciplina[disciplinaEscolhida - 1].codigo == 0 || disciplinaEscolhida == 0) { // código tá vazando e deixando o 0 passar, tentarei consertar no futuro
+              printf("\n\nEssa disciplina não existe\n(Aperte Enter para continuar)\n\n");
+              getchar();
+            } 
+            else {
+              system("clear");
+              puts(disciplina[disciplinaEscolhida - 1].nome);
+              printf("- Código: %d\n- Semestre: %d\n- Professor: ", disciplina[disciplinaEscolhida - 1].codigo, disciplina[disciplinaEscolhida - 1].semestre);
+              puts(disciplina[disciplinaEscolhida - 1].professor);
+
+              if (disciplina[disciplinaEscolhida - 1].alunos[0][0] != '\0') {
+                printf("- Alunos Cadastrados na Disciplina: \n");
+
+                for (int j = 0; j < usuariosQtd; j++) {
+                  if (disciplina[disciplinaEscolhida - 1].alunos[j][0] != '\0') {
+                    printf("  %d. ", j + 1);
+                    puts(disciplina[disciplinaEscolhida - 1].alunos[j]);
+                  }
+                }
+
+                printf("\n\n");
+              } 
+              else {
+                printf("- Não há alunos cadastrados nessa disciplina\n\n\n");
+              }
+
+              printf("(Aperte Enter para continuar)\n\n");
+              getchar();
+            }
+          }
         }
         // Listar alunos por gênero
         else if (escolhaRelatorio == 5) {
