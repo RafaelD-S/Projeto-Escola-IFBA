1872fd65cd2b5ae895f04cdef09b14f9b8501785
Luana Lima <luanalima.2006@hotmail.com>
2024-10-28 17:11:57 -0300

    feat: relatório dos aniversariantes do mês

--- main.c
+++ main.c
@@ -16,27 +16,27 @@ int main() {
   alunos[0].matricula = 123;
   alunos[0].sexo = 'F';
   strcpy(alunos[0].nome, nome1); // nome do aluno
-  alunos[0].dtNasc = 999999;
+  alunos[0].dtNasc = 28032006;
   alunos[0].cpf = 12345678912;
 
   alunos[1].matricula = 123;
   alunos[1].sexo = 'M';
   strcpy(alunos[1].nome, nome5); // nome do aluno
-  alunos[1].dtNasc = 999999;
+  alunos[1].dtNasc = 18012006;
   alunos[1].cpf = 12345678912;
 
   // Professor 1
   prof[0].matricula = 123;
   prof[0].sexo = 'F';
   strcpy(prof[0].nome, nome2); // nome do professor 1
-  prof[0].dtNasc = 999999;
+  prof[0].dtNasc = 18012006;
   prof[0].cpf = 12345678912;
 
   // Professor 2
   prof[1].matricula = 123;
   prof[1].sexo = 'M';
   strcpy(prof[1].nome, nome3); // nome do professor 2
-  prof[1].dtNasc = 999999;
+  prof[1].dtNasc = 18122006;
   prof[1].cpf = 12345678912;
 
   strcpy(disciplina[0].nome, nome4); // nome da matéria
@@ -1260,7 +1260,83 @@ int main() {
         }
         // Listar aniversariantes do mês
         else if (escolhaRelatorio == 11) {
-          printf("%d", escolhaRelatorio);
+                    int escolhaMes;
+          while (true) {
+            system("clear");
+            int temPessoa = false;
+            for (int i = usuariosQtd - 1; i >= 0; i--) {
+              if (alunos[i].matricula == 0 && prof[i].matricula == 0 )
+                continue;
+
+              else 
+                temPessoa = true;
+            }
+
+            if (!temPessoa) {
+              printf("\n\nNÃO HÁ ALUNOS DISPONÍVEIS\n(Aperte Enter para continuar)\n\n");
+              getchar();
+            } 
+            else {
+              system("clear");
+              printf("\nANIVERSARIANTES DO MÊS");
+              printf("\nEscolha um mês para ver os aniversariantes:\n\n");
+              printf("[1] - Janeiro\n");
+              printf("[2] - Fevereiro\n");
+              printf("[3] - Março\n");
+              printf("[4] - Abril\n");
+              printf("[5] - Maio\n");
+              printf("[6] - Junho\n");
+              printf("[7] - Julho\n");
+              printf("[8] - Agosto\n");
+              printf("[9] - Setembro\n");
+              printf("[10] - Outubro\n");
+              printf("[11] - Novembro\n");
+              printf("[12] - Dezembro\n");    
+              printf("\nInsira qualquer outra tecla para voltar\n");
+              printf("\n=> ");
+              scanf("%d", &escolhaMes);
+              getchar();
+
+              if (escolhaMes <= 12 && escolhaMes >= 1) {
+                system("clear");           
+                int temAniversariante = false; 
+                for (int j = 0; j < usuariosQtd; j++) {
+                  if (alunos[j].matricula != 0) {
+                    int mesAluno;
+                    mesAluno = alunos[j].dtNasc / 10000;
+                    mesAluno = mesAluno % 100;
+
+                    if (escolhaMes == mesAluno) {
+                      temAniversariante = true;
+                      printf("- Discente ");
+                      puts(alunos[j].nome);
+                    }
+                  }
+
+                  if (prof[j].matricula != 0) {
+                    int mesProf;
+                    mesProf = prof[j].dtNasc / 10000;
+                    mesProf = mesProf % 100;
+                    if (escolhaMes == mesProf) {
+                      temAniversariante = true;
+                      printf("- Docente ");
+                      puts(prof[j].nome);
+                    }
+                  }
+                }
+
+                if (!temAniversariante) {
+                  printf("NÃO HÁ ANIVERSARIANTES NESSE MÊS\n(Aperte Enter para continuar)\n\n");
+                  getchar();
+                  break;
+                }
+
+                printf("\n\nAniversariantes listados com sucesso\n(Aperte Enter para continuar)\n\n");
+                getchar();
+              }
+              break;
+            }
+          } 
         }
         // Pesquisar pesssoas
         else if (escolhaRelatorio == 12) {
